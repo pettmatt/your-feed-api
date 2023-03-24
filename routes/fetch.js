607922132c78c7806fa224681ff0623b10b01dbd
Petteri Mattila <petterimattila4@outlook.com>
2023-03-24 11:45:26 +0200

    scraper can filter old articles if the article element contains a time element with 'datetime' attribute

--- routes/fetch.js
+++ routes/fetch.js
@@ -2,14 +2,16 @@ const scrapeArticle = require("../services/scraper")
 const express = require("express")
 const router = express.Router()
 
-router.get("/fetch/:url", async (req, res, next) => {
+router.get("/fetch/:url/:ignoreDate", async (req, res, next) => {
     const url = (
         req.params.url.includes("https://www.") ||
         req.params.url.includes("http://www.") 
     ) ? req.params.url
     : `http://www.${ req.params.url }`
 
-    const articles = await scrapeArticle(url)
+    const ignoreBeforeDate = req.params.ignoreDate
+    console.log("Ignore", ignoreBeforeDate)
+    const articles = await scrapeArticle(url, ignoreBeforeDate)
 
     return res.status(200).json({
         message: `Hello from fetch!`,
--- services/scraper.js
+++ services/scraper.js
@@ -1,11 +1,14 @@
 const puppeteer = require("puppeteer")
 
-const scrapeArticle = async (url) => {
+const scrapeArticle = async (url, ignoreBeforeDate = null) => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.goto(url)
 
-    const articles = await page.evaluate(() =>
+    // The ignoreBeforeDate value is passed as the second argument to the function.
+    // Without including the variable, the program is going to prompt an error, which tells the variable haven't been defined.
+    // Read more about this from Puppeteer docs: https://pptr.dev/api/puppeteer.page.evaluate
+    const articles = await page.evaluate((ignoreBeforeDate) =>
         Array
             .from(document.querySelectorAll(".article"))
             .map(article => {
@@ -16,8 +19,16 @@ const scrapeArticle = async (url) => {
                         alt: ""
                     },
                     header: "",
-                    paragraph: ""
+                    paragraph: "",
+                    published: null
                 }
+
+                object.published = article.querySelector("time").getAttribute("datetime")
+
+                // Check if the article is out of scope. There is option to ignore articles before a date.
+                if ( ignoreBeforeDate !== null && object.published !== null )
+                    if ( new Date(object.published) < new Date(ignoreBeforeDate) )
+                        return
 
                 object.url = article.querySelector("a").getAttribute("href")
                 object.img.src = article.querySelector("img").getAttribute("href")
@@ -26,7 +37,7 @@ const scrapeArticle = async (url) => {
                 object.paragraph = article.querySelector("p").textContent.trim()
 
                 return object
-            })
+            }), ignoreBeforeDate
     )
 
     browser.close()
