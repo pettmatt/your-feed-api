b017d2ed8035fa4fb5ec7c5db1fc870573aec920
Petteri Mattila <petterimattila4@outlook.com>
2023-04-24 15:01:24 +0300

    replacing 'serverless-chrome/lambda' package with 'chrome-aws-lambda'

--- services/scraper.js
+++ services/scraper.js
@@ -1,46 +1,15 @@
+const { ssr } = require("./test.js")
 const puppeteer = require("puppeteer")
 
+let WSEndpoint = null
+
 const scrapeArticles = async (url, ignoreBeforeDate = null) => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto(url)
+    if ( !WSEndpoint ) {
+        const browser = await puppeteer.launch()
+        WSEndpoint = browser.wsEndpoint()
+    }
 
-    // The ignoreBeforeDate value is passed as the second argument to the function.
-    // Without including the variable, the program is going to prompt an error, which tells the variable haven't been defined.
-    // Read more about this from Puppeteer docs: https://pptr.dev/api/puppeteer.page.evaluate
-    // const articles = await page.evaluate((ignoreBeforeDate) =>
-    //     Array
-    //         .from(document.querySelectorAll(".article"))
-    //         .map(article => {
-    //             const object = {
-    //                 url: "",
-    //                 img: {
-    //                     src: null,
-    //                     alt: ""
-    //                 },
-    //                 header: "",
-    //                 paragraph: "",
-    //                 published: null
-    //             }
-
-    //             object.published = article.querySelector("time").getAttribute("datetime")
-
-    //             // Check if the article is out of scope. There is an option to ignore articles based on the date.
-    //             if ( ignoreBeforeDate !== null && object.published !== null )
-    //                 if ( new Date(object.published) < new Date(ignoreBeforeDate) )
-    //                     return
-
-    //             object.url = article.querySelector("a").getAttribute("href")
-    //             object.img.src = article.querySelector("img").getAttribute("href")
-    //             object.img.alt = article.querySelector("img").getAttribute("alt")
-    //             object.header = article.querySelector("h3").textContent.trim()
-    //             object.paragraph = article.querySelector("p").textContent.trim()
-
-    //             return object
-    //         }), ignoreBeforeDate
-    // )
-
-    browser.close()
+    const articles = ssr(url, ignoreBeforeDate, WSEndpoint)
 
     return articles
 }
@@ -75,40 +44,6 @@ const scrapeArticleSnippet = async (url) => {
     browser.close()
 
     return articles
-}
-
-const basicScraping = async (page) => {
-    await page.evaluate((ignoreBeforeDate) =>
-        Array
-            .from(document.querySelectorAll(".article"))
-            .map(article => {
-                const object = {
-                    url: "",
-                    img: {
-                        src: null,
-                        alt: ""
-                    },
-                    header: "",
-                    paragraph: "",
-                    published: null
-                }
-
-                object.published = article.querySelector("time").getAttribute("datetime")
-
-                // Check if the article is out of scope. There is an option to ignore articles based on the date.
-                if ( ignoreBeforeDate !== null && object.published !== null )
-                    if ( new Date(object.published) < new Date(ignoreBeforeDate) )
-                        return
-
-                object.url = article.querySelector("a").getAttribute("href")
-                object.img.src = article.querySelector("img").getAttribute("href")
-                object.img.alt = article.querySelector("img").getAttribute("alt")
-                object.header = article.querySelector("h3").textContent.trim()
-                object.paragraph = article.querySelector("p").textContent.trim()
-
-                return object
-            }), ignoreBeforeDate
-    )
 }
 
 module.exports = { scrapeArticles, scrapeArticleSnippet }
